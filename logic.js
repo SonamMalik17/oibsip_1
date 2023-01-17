6fff3ded8a1f0a366fe94fa2f9bd98ada68c5961
SonamMalik17 <maliksonam0301@gmail.com>
2023-01-18 00:29:15 +0530

    Basic Logic added

---
+++ logic.js
@@ -0,0 +1,32 @@
+var screen=document.getElementById("screen");
+var buttons=document.querySelectorAll("button");
+var screenValue=' ';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        var buttonText=e.target.innerText;
+        console.log(buttonText);
+        if(buttonText=='x'){
+            buttonText='*';
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+        else if(buttonText=='C'){
+            screenValue=' ';
+            screen.value=screenValue;
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
+
+
+
+
+
+
+
